d2afb6134b74233f79b718d3edb81ca8c1f21ca6
Brandon Holt <brandonholt95@gmail.com>
2021-06-29 10:59:47 -0400

    Clean up

--- ElutionModel.m
+++ ElutionModel.m
@@ -1,25 +1,21 @@
 function [time, bound] = ElutionModel(nCaptureAntibodies, nProtein, nSignalAntibodies, nCompetingAntibodies, nExcessProtein, kon, koff)
 
-    % element-1 = target protein + capture Ab
-    % element-2 = target protein + signal Ab
-    % element-3 = target protein + competing Ab
-    global Kon; Kon = kon;
-    global Koff; Koff = koff;
+    global Kon; Kon = kon; % [Kcapture, Ksignal, Kcompeting]
+    global Koff; Koff = koff; % [Kcapture, Ksignal, Kcompeting]
     
-    % preallocate outputs
+    % preallocate output arrays
     global tStep; tStep = 0.01;
     t = 0; tMax = 30;
     time = (0:tStep:tMax - tStep)';
     bound = zeros(numel(time),1);
     step = 1;
     
-    % starting at moment where all nProtein are bound to signal and capture Ab
-    % 0 = unbound, 1 = bound to capture Ab, 2 = bound to signal, 3 = bound to competing
-    global protein; valency = 2;
-    protein = zeros(nProtein + nExcessProtein, valency);
-    protein(1:nProtein, 1) = 1;
-    protein(1:nProtein, 2) = 2;
-    % for antibodies 3 columns: 1 = current state, 2 = goal state, 3 = time remaining
+    % protein -> 0 = unbound, 1 = bound to capture Ab, 2 = bound to signal, 3 = bound to competing
+    global protein; valency = 2; 
+    protein = zeros(nProtein + nExcessProtein, valency); % add excess protein
+    protein(1:nProtein, 1) = 1; % all bound to one capture Ab
+    protein(1:nProtein, 2) = 2; % all bound to one signal Ab
+
     % captureAntibodies -> 0 = unbound, i = linear index of protein bound to
     captureAntibodies = zeros(nCaptureAntibodies,3);
     captureAntibodies(1:min(nProtein, nCaptureAntibodies),1) = 1:min(nProtein, nCaptureAntibodies);
@@ -40,11 +36,9 @@ function [time, bound] = ElutionModel(nCaptureAntibodies, nProtein, nSignalAntib
         
         % update state of all antibodies
         for i = randperm(max([length(signalAntibodies), length(competingAntibodies), length(captureAntibodies)]))
-
             captureAntibodies = UpdateAntibodyState(captureAntibodies, i, Antibodies.Capture);
             signalAntibodies = UpdateAntibodyState(signalAntibodies, i, Antibodies.Signal);
             competingAntibodies = UpdateAntibodyState(competingAntibodies, i, Antibodies.Competing);
-
         end
 
         % increment time
@@ -61,32 +55,33 @@ function [ab] = UpdateAntibodyState(ab, i, i_ab)
     
     global protein; global tStep;
     global Kon; global Koff;
+    current = 1; goal = 2; time = 3;
     
-    if ab(i,2) ~= -1 % if action is in progress continue
+    if ab(i,goal) ~= -1 % if action is in progress continue
 
-        ab(i,3) = ab(i,3) - tStep;
-        if ab(i,3) <= 0 % finished, complete and reset
-            if ab(i,1) > 0; protein(ab(i,1)) = 0; end % unbind the protein as well
-            ab(i,1) = ab(i,2);
-            ab(i,2) = -1;
+        ab(i,time) = ab(i,time) - tStep;
+        if ab(i,time) <= 0 % finished, complete and reset
+            if ab(i,current) > 0; protein(ab(i,current)) = 0; end % unbind the protein as well
+            ab(i,current) = ab(i,goal);
+            ab(i,goal) = -1;
         end
 
-    elseif ab(i,1) == 0 % if currently unbound
+    elseif ab(i,current) == 0 % if currently unbound
 
         % bind antibody
         free_proteins = find(protein == 0);
-        if i_ab == Antibodies.Capture; free_proteins = free_proteins(free_proteins <= length(protein)); end % capture abs only bind to first position
+        if i_ab == Antibodies.Capture; free_proteins = free_proteins(free_proteins <= length(protein)); end % capture abs only bind to first position, ensure there is only one capture Ab per protein
         if numel(free_proteins) == 0; return; end
         free_protein = free_proteins(randi([1, length(free_proteins)]));
-        ab(i,2) = free_protein;
-        ab(i,3) = exprnd(1 / Kon(i_ab));
+        ab(i,goal) = free_protein;
+        ab(i,time) = exprnd(1 / Kon(i_ab));
         % bind protein
         protein(free_protein) = i_ab;
 
-    elseif ab(i,1) > 0 % if currently bound, begin unbinding
+    elseif ab(i,current) > 0 % if currently bound, begin unbinding
 
-        ab(i,2) = 0;
-        ab(i,3) = exprnd(1 / Koff(i_ab));
+        ab(i,goal) = 0;
+        ab(i,time) = exprnd(1 / Koff(i_ab));
 
     end
 
